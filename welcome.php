8aa571e831fa35f9fab27070d32d67af56134355
krisjanee <cabilankrisjane@gmail.com>
2023-06-25 10:28:09 -0700

    add logout page

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
+    header("location: welcome.php");
     exit;
 }
 ?>
@@ -13,18 +13,117 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>Admin</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+<meta charset="UTF-8">
+    <title>Student Grading</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Student Grading</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/student-grade.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link rel="shortcut icon" href="assets/img/ronk1.jpg" />
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet"> -->
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style22.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <a class="navbar-brand" href="index.php"><img src="assets/img/student-grade.png" alt="Logo" style="width:30px;height:30px;"></a>
+      <h1 class="logo mr-auto"><a href="index.php">Student Grading System</a></h1> 
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+
+    </div>
+  </header><!-- End Header -->
+
+</head>
+<body>
+  <br>
+  <br>
+  <br>
+
+<?php
+                    // Include config file
+                    require_once "config.php";
+
+
+  // Attempt select query execution
+$sql = "SELECT * FROM students";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo '<table class="table table-bordered table-striped">';
+            echo "<tr>";
+            echo '<tr class="bg-primary">';
+                echo "<th>id</th>";
+                echo "<th>name</th>";
+                echo "<th>calculus</th>";
+                echo "<th>discrete</th>";
+                echo "<th>programming</th>";
+                echo "<th>algorithms</th>";
+                echo "<th>applicaton</th>";
+                echo "<th>total</th>";
+                echo "<th>grade</th>";
+            echo "</tr>";
+            while($row = mysqli_fetch_array($result)){
+                echo "<tr>"; 
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['name'] . "</td>";
+                    echo "<td>" . $row['calculus'] . "</td>";
+                    echo "<td>" . $row['discrete'] . "</td>";
+                    echo "<td>" . $row['programming'] . "</td>";
+                    echo "<td>" . $row['algorithms'] . "</td>";
+                    echo "<td>" . $row['application'] . "</td>";
+                    echo "<td>" . $row['total'] . "</td>";
+                    echo "<td>" . $row['grade'] . "</td>";
+                echo "</tr>";
+            }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
+
+<div class="text-center">
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
+</div>
 </body>
-
-
+</html>
